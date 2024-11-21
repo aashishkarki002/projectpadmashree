9a983390f72a332db93b0c7d1114e82deacef8ef
aashishkarki002 <karkiaashish6002@gmail.com>
2024-11-21 19:16:57 +0545

    images

--- Frontend/signup.php
+++ Frontend/signup.php
@@ -11,6 +11,7 @@ include("../backend/connect.php");
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Amiko:wght@400;600;700&display=swap" rel="stylesheet">
+<script src="authentication.js"></script>
 </head>
 <body>
     <div class="main">
